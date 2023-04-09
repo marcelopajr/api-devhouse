f1ca976462393fa38d4ef07ee1c72de5e7fecccc
marcelopajr <marcelo96jr@gmail.com>
2023-04-09 18:30:15 -0300

    chore: add nodemon and sucrase

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require("express");
+import { Router } from "express";
 
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ ok: true });
+  return res.json({ status: "Server online" });
 });
 
-module.exports = routes;
+export default routes;
