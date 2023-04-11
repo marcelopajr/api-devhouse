607de835df7ea4b6476bab67c6ba23666b6a46f2
marcelopajr <marcelo96jr@gmail.com>
2023-04-11 14:45:54 -0300

    feat: add session validation with yup

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,8 +1,18 @@
 import User from "../models/User";
+import * as Yup from "yup";
 
 class SessionController {
   async store(req, res) {
+    const sessionSchema = Yup.object().shape({
+      email: Yup.string().email().required(),
+    });
+
     const { email } = req.body;
+
+    // Validate email
+    if (!(await sessionSchema.isValid(req.body))) {
+      return res.status(400).json({ error: "Error validating email!" });
+    }
 
     // Verify if user already exist
     let user = await User.findOne({ email });
